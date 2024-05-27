0811363de3b472ec725c925501248b839ca207fb
echodrift <luuvanducthieu291@gmail.com>
2024-05-27 17:40:56 +0700

    Update main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ import java.util.Hashtable;
 
 
 public class Main {
-    private static String parseFile(String projectName, String projectDir, String filePath) {
+    private static String parseFile(String projectName, String projectDir, String filePath, String className) {
         Config.autoConfigure(projectName, projectDir);
         ASTParser parser = ASTParser.newParser(Config.JDT_LEVEL);
         parser.setResolveBindings(true);
@@ -27,31 +27,40 @@ public class Main {
         parser.setSource(FileProcessor.read(new File(filePath)).toCharArray());
         CompilationUnit cu = (CompilationUnit) parser.createAST(null);
         int numClass = cu.types().size();
-        TypeDeclaration targetClass;
-        if (numClass > 0) {
-            try {
-                targetClass = (TypeDeclaration) cu.types().get(0);
-            } catch (Exception e) {
-                return "<enum>";
-            }
-            ITypeBinding binding = targetClass.resolveBinding();
-            if (binding == null) {
-                return "<binding>";
-            }
-            ITypeBinding superClass = binding.getSuperclass();
-            if (superClass == null) {
-                return "<object>";
-            }
-            String superClassName = superClass.getQualifiedName();
-            if (superClassName.isEmpty() || superClassName.equals("java.lang.Object") || superClassName.equals("null")) {
-                return "<no_super_class>";
+        TypeDeclaration targetClass = null;
+        try {
+            if (numClass > 0) {
+                for (int i = 0; i < numClass; i++) {
+                    TypeDeclaration type = (TypeDeclaration) cu.types().get(i);
+                    if (numClass == 1 || type.getName().toString().equals(className)) {
+                        targetClass = type;
+                        break;
+                    }
+                }
+                if (targetClass == null) {
+                    targetClass = (TypeDeclaration) cu.types().get(0);
+                }
             } else {
-                String[] methods = Arrays.stream(targetClass.resolveBinding().getSuperclass().getDeclaredMethods()).map(Object::toString).filter(str -> str.contains("public")).toArray(String[]::new);
-                String[] vars = Arrays.stream(targetClass.resolveBinding().getSuperclass().getDeclaredFields()).map(Object::toString).toArray(String[]::new);
-                return "<methods>" + String.join(",", methods) + "<variables>" + String.join(",", vars);
+                return "<no_class>";
             }
+        } catch (Exception e) {
+            return "<enum>";
+        }
+        ITypeBinding binding = targetClass.resolveBinding();
+        if (binding == null) {
+            return "<binding>";
+        }
+        ITypeBinding superClass = binding.getSuperclass();
+        if (superClass == null) {
+            return "<object>";
+        }
+        String superClassName = superClass.getQualifiedName();
+        if (superClassName.isEmpty() || superClassName.equals("java.lang.Object") || superClassName.equals("null")) {
+            return "<no_super_class>";
         } else {
-            return "<no_class>";
+            String[] methods = Arrays.stream(targetClass.resolveBinding().getSuperclass().getDeclaredMethods()).map(Object::toString).filter(str -> str.contains("public")).toArray(String[]::new);
+            String[] vars = Arrays.stream(targetClass.resolveBinding().getSuperclass().getDeclaredFields()).map(Object::toString).toArray(String[]::new);
+            return "<methods>" + String.join(",", methods) + "<variables>" + String.join(",", vars);
         }
     }
 
@@ -60,12 +69,13 @@ public class Main {
         String baseDir = args[0];
         String projName = args[1];
         String relativePath = args[2];
+        String className = args[3];
         String projDir = baseDir + "/" + projName;
         String filePath = projDir + "/" + relativePath;
         try {
-            System.out.println(parseFile(projName, projDir, filePath));
+            System.out.println(parseFile(projName, projDir, filePath, className));
         } catch (Exception e) {
-            System.err.println("Encounter error");
+            System.out.println("<encounter_error>");
         }
     }
 }
